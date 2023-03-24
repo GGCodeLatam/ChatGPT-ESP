b61fe07a9a822d15d23c597b07e0c5b75d98a050
GG Code <gg.code.latam@gmail.com>
2023-03-24 18:25:23 -0300

    version 0.1.0

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -8,13 +8,20 @@ import { db } from "../firebase";
 import { toast } from "react-hot-toast";
 import ModelSelection from "./ModelSelection";
 import useSWR from "swr";
-import ChatRecord from "./ChatRecord"
+import ChatRecord from "./ChatRecord";
 
 type Props = {
   chatId: string;
 };
 
 function ChatInput({ chatId }: Props) {
+  const [exportedTranscription, setExportedTranscription] = useState<string | null>(null);
+
+  const handleTranscriptionExported = (transcription: string) => {
+    setExportedTranscription(transcription);
+  };
+
+  const [userInput, setUserInput] = useState('');
   const [prompt, setPrompt] = useState<string>("");
   const { data: session } = useSession();
 
@@ -22,6 +29,8 @@ function ChatInput({ chatId }: Props) {
   const { data: model } = useSWR("model", {
     fallbackData: "text-davinci-003",
   });
+
+
 
   const sendMessage = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -40,6 +49,11 @@ function ChatInput({ chatId }: Props) {
           session?.user?.image! ||
           `https://ui-avatars.com/api/?name=${session?.user?.name}`,
       },
+    };
+    const handleSubmit = (e: any) => {
+      e.preventDefault();
+      const finalPrompt = exportedTranscription ? preprompt + exportedTranscription + prepregunta + userInput : pregunta1;
+      // Lógica para enviar el formulario con el prompt final
     };
 
     await addDoc(
@@ -75,31 +89,81 @@ function ChatInput({ chatId }: Props) {
       });
     });
   };
+  console.log(exportedTranscription)
+
+
+  const preprompt = "A continuación, te muestro el texto de una grabación para que luego contestes una pregunta sobre el mismo, la grabacion es esta: '"
+  const prepregunta = " Ahora, la pregunta a responder sobre la grabación anterior es: "
+  const pregunta1 = "' Cual es el foco de lo que intenta expresar esta grabación? "
+  const pregunta2 = "' En la grabación que sentimientos y / o emociones se pueden sentir? "
+  const pregunta3 = "' Segun los escritos de Freud, que se puede observar en lo dicho en esta grabación? "
+  const pregunta4 = " ' Segun la psicología moderna, que se podría analizar de esta grabación?"
+
 
   return (
     <div className="bg-gray-700/50 text-gray-400 rounded-lg text-sm ">
-      <form onSubmit={sendMessage} className="p-5 space-x-5 flex">
-        <ChatRecord />
+      <form onSubmit={sendMessage} className="p-5 space-x-5 flex justify-center items-center">
+        <div className="justify-start">
+          <ChatRecord onExport={handleTranscriptionExported} />
+        </div>
+        <div className="flex flex-col gap-3">
+          <button
+            className="bg-gray-900 hover:bg-gray-800 text-white py-2 px-4 rounded h-10"
+            value={exportedTranscription?.toString()}
+            onClick={() => setPrompt(exportedTranscription ? preprompt + exportedTranscription + prepregunta + pregunta1 : pregunta1)}
+          >
+            De qué habla?
+          </button>
+          <button
+            className="bg-gray-900 hover:bg-gray-800 text-white py-2 px-4 rounded h-10"
+            value={exportedTranscription?.toString()}
+            onClick={() => setPrompt(exportedTranscription ? preprompt + exportedTranscription + prepregunta + pregunta2 : pregunta2)}
+          >
+            Sentimientos / Emociones
+          </button>
+          <button
+            className="bg-gray-900 hover:bg-gray-800 text-white py-2 px-4 rounded h-10"
+            value={exportedTranscription?.toString()}
+            onClick={() => setPrompt(exportedTranscription ? preprompt + exportedTranscription + prepregunta + pregunta3 : pregunta3)}
+          >
+            Psicoanalisis
+          </button>
+          <button
+            className="bg-gray-900 hover:bg-gray-800 text-white py-2 px-4 rounded h-10"
+            value={exportedTranscription?.toString()}
+            onClick={() => setPrompt(exportedTranscription ? preprompt + exportedTranscription + prepregunta + pregunta4 : pregunta4)}
+          >
+            Psicologia moderna
+          </button>
+        </div>
         <input
           className="bg-transparent focus:outline-none flex-1 disabled:cursor-not-allowed disabled:text-gray-300"
           disabled={!session}
-          value={prompt}
-          onChange={(e) => setPrompt(e.target.value)}
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)}
           type="text"
-          placeholder="Type your message here..."
+          placeholder="Formula tu propia pregunta sobre la grabación aquí..."
+          enterKeyHint="enter"
         />
         <button
+          className="bg-gray-900 hover:bg-gray-800 text-white py-2 px-4 rounded h-10"
+          value={exportedTranscription?.toString()}
+          onClick={() => setPrompt(exportedTranscription ? preprompt + exportedTranscription + prepregunta + userInput : pregunta4) && setUserInput('')}
+        >
+          Pregunta personalizada
+        </button>
+{/*         <button
           disabled={!prompt || !session}
           type="submit"
           className="bg-[#11A37F] hover:opacity-50 text-white font-bold 
           px-4 py-2 rounded disabled:bg-gray-300 disabled:cursor-not-allowed"
         >
           <PaperAirplaneIcon className="h-4 w-4 -rotate-45" />
-        </button>
+        </button> */}
       </form>
 
       {/* ModelSelection */}
-      <div>
+      <div className="hidden">
         <ModelSelection />
       </div>
     </div>
--- components/ChatRecord.tsx
+++ components/ChatRecord.tsx
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from 'react';
 
-declare global {
-    interface Window {
-      SpeechRecognition: any;
-      webkitSpeechRecognition: any;
-    }
-  }
+import { MicrophoneIcon } from "@heroicons/react/24/solid";
 
+declare global {
+  interface Window {
+    SpeechRecognition: any;
+    webkitSpeechRecognition: any;
+  }
+}
 
 const SpeechRecognition: typeof window.SpeechRecognition =
-    window?.SpeechRecognition || window?.webkitSpeechRecognition;
+  window?.SpeechRecognition || window?.webkitSpeechRecognition;
 
 const mic = new SpeechRecognition();
 
@@ -18,85 +19,110 @@ mic.interimResults = true;
 mic.lang = 'es-ES';
 
 interface ISpeechRecognitionEvent extends Event {
-    results: {
-        transcript: string;
-        confidence: number;
-    }[];
+  results: {
+    transcript: string;
+    confidence: number;
+  }[];
 }
 
-interface ISpeechToTextProps { }
+interface ISpeechToTextProps {
+  onExport: (transcription: string) => void;
+}
 
-const SpeechToText: React.FC<ISpeechToTextProps> = () => {
-    const [isListening, setIsListening] = useState(false);
-    const [note, setNote] = useState<string | null>(null);
-    const [savedNotes, setSavedNotes] = useState<string[]>([]);
+const SpeechToText: React.FC<ISpeechToTextProps> = ({ onExport }) => {
+  const [isListening, setIsListening] = useState(false);
+  const [transcription, setTranscription] = useState<string | null>(null);
+  const [exportedTranscription, setExportedTranscription] = useState<string | null>(null);
 
-    useEffect(() => {
-        handleListen();
-    }, [isListening]);
+  useEffect(() => {
+    handleListen();
+  }, [isListening]);
 
-    const handleListen = (): void => {
-        if (isListening) {
-            mic.start();
-            mic.onend = () => {
-                console.log('continue..');
-                mic.start();
-            };
-        } else {
-            mic.stop();
-            mic.onend = () => {
-                console.log('Stopped Mic on Click');
-            };
-        }
-        mic.onstart = () => {
-            console.log('Mics on');
-        };
-
-        mic.onresult = (event: ISpeechRecognitionEvent) => {
-            const transcript = Array.from(event.results)
-                .map((result: unknown) => (result as SpeechRecognitionResult)[0])
-                .map((result: any) => result.transcript)
-                .join('');
-            console.log(transcript);
-            setNote(transcript);
-        };
-
-        mic.onerror = (event: { error: any; }) => {
-            console.log(event.error);
-        };
+  const handleListen = (): void => {
+    if (isListening) {
+      mic.start();
+      mic.onend = () => {
+        console.log('continue..');
+        mic.start();
+      };
+    } else {
+      mic.stop();
+      mic.onend = () => {
+        console.log('Stopped Mic on Click');
+      };
+    }
+    mic.onstart = () => {
+      console.log('Mics on');
     };
 
-    const handleSaveNote = (): void => {
-        if (note) {
-            setSavedNotes([...savedNotes, note]);
-            setNote(null);
-        }
+    mic.onresult = (event: ISpeechRecognitionEvent) => {
+      const transcript = Array.from(event.results)
+        .map((result: unknown) => (result as SpeechRecognitionResult)[0])
+        .map((result: any) => result.transcript)
+        .join('');
+      console.log(transcript);
+      setTranscription(transcript);
     };
 
-    return (
-        <>
-            <h1>Voice Notes</h1>
-            <div className="container">
-                <div className="box">
-                    <h2>Current Note</h2>
-                    {isListening ? <span>🎙️</span> : <span>🛑🎙️</span>}
-                    <button onClick={handleSaveNote} disabled={!note}>
-                        Save Note
-                    </button>
-                    <button onClick={() => setIsListening(prevState => !prevState)}>
-                        Start/Stop
-                    </button>
-                    <p>{note}</p>
-                </div>
-                <div className="box">
-                    <h2>Notes</h2>
-                    {savedNotes.map(n => (
-                        <p key={n}>{n}</p>
-                    ))}
-                </div>
+    mic.onerror = (event: { error: any }) => {
+      console.log(event.error);
+    };
+  };
+
+  const handleExportTranscription = async (): Promise<void> => {
+    if (transcription) {
+      setExportedTranscription(transcription);
+      onExport(transcription);
+    }
+  };
+
+  return (
+    <>
+      <div className="container mx-auto my-8">
+        <div className="bg-gray-900 rounded-lg p-4 max-w-md mx-auto">
+          <div className="flex items-center mb-4 gap-4">
+            {isListening ? (
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 3.75V16.5L12 14.25 7.5 16.5V3.75m9 0H18A2.25 2.25 0 0120.25 6v12A2.25 2.25 0 0118 20.25H6A2.25 2.25 0 013.75 18V6A2.25 2.25 0 016 3.75h1.5m9 0h-9" />
+              </svg>
+
+            ) : (
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 18.75a6 6 0 006-6v-1.5m-6 7.5a6 6 0 01-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 01-3-3V4.5a3 3 0 116 0v8.25a3 3 0 01-3 3z" />
+              </svg>
+
+            )}
+
+            <button className="bg-gray-700 hover:bg-gray-800 text-white py-2 px-4 rounded mr-4" onClick={() => setIsListening((prevState) => !prevState)}>
+              Start/Stop
+            </button>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062A1.125 1.125 0 013 16.81V8.688zM12.75 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062a1.125 1.125 0 01-1.683-.977V8.688z" />
+              </svg>
+            <button className="bg-gray-700 hover:bg-gray-800 text-white py-2 px-4 rounded" onClick={handleExportTranscription} disabled={!transcription}>
+              Export Transcription
+            </button>
+          </div>
+
+          <p className="text-gray-400">{transcription}</p>
+        </div>
+
+        <div className="bg-gray-900 rounded-lg p-4 max-w-md mx-auto mt-8">
+          <h2 className="text-gray-400 mb-4">Exported Transcription</h2>
+
+          {exportedTranscription ? (
+            <div>
+              <p className="text-gray-400 mb-4">{exportedTranscription}</p>
+              <button className="bg-gray-700 hover:bg-gray-800 text-white py-2 px-4 rounded" onClick={() => setExportedTranscription(null)}>Clear</button>
             </div>
-        </>
-    );
+          ) : (
+            <p className="text-gray-400">No exported transcription yet.</p>
+          )}
+        </div>
+      </div>
+
+    </>
+  );
 };
 
 export default SpeechToText;
